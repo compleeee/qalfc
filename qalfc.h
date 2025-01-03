20c6481c2b98b3de482ca43a1b64872ca4d7cda4
compleeee <125695064+compleeee@users.noreply.github.com>
2025-01-03 20:33:32 +0100

    qalfc.h
    added double float, short and long integer

--- qalfc.h
+++ qalfc.h
@@ -4,9 +4,26 @@ float fsub(float a, float b);
 float fmol(float a, float b);
 float fdiv(float a, float b);
 
+double dsum(double a, double b);
+double dsub(double a, double b);
+double dmol(double a, double b);
+double ddiv(double a, double b);
+
 /* INTEGRES NUMBER */
 int sum(int a, int b);
 int sub(int a, int b);
 int mol(int a, int b);
 int div(int a, int b);
 int rest(int a, int b);
+
+long lsum(long a, long b);
+long lsub(long a, long b);
+long lmol(long a, long b);
+long ldiv(long a, long b);
+long lrest(long a, long b);
+
+short ssum(short a, short b);
+short ssub(short a, short b);
+short smol(short a, short b);
+short sdiv(short a, short b);
+short srest(short a, short b);
