24d2f668fb805a39feb57960f9f5a0b730139764
compleeee <125695064+compleeee@users.noreply.github.com>
2025-01-03 19:50:21 +0100

    Add files via upload

---
+++ qalfc.c
@@ -0,0 +1,44 @@
+float fsum(float a, float b)
+{
+    return a + b;
+}
+
+float fsub(float a, float b)
+{
+    return a - b;
+}
+
+float fmol(float a, float b)
+{
+    return a * b;
+}
+
+float fdiv(float a, float b)
+{
+    return a / b;
+}
+
+int sum(int a, int b)
+{
+    return a + b;
+} 
+
+int sub(int a, int b)
+{
+    return a - b;
+}
+
+int mol(int a, int b)
+{
+    return a * b;
+}
+
+int div(int a, int b)
+{
+    return a / b;
+}
+
+int rest(int a, int b)
+{
+    return a % b;
+}
---
+++ qalfc.h
@@ -0,0 +1,12 @@
+/* FLOAT NUMBERS */
+float fsum(float a, float b);
+float fsub(float a, float b);
+float fmol(float a, float b);
+float fdiv(float a, float b);
+
+/* INTEGRES NUMBER */
+int sum(int a, int b);
+int sub(int a, int b);
+int mol(int a, int b);
+int div(int a, int b);
+int rest(int a, int b);
