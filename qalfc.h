0f14ecdf49d3c183abca4af139a18afc4b01e5fd
compleeee <125695064+compleeee@users.noreply.github.com>
2025-01-04 18:02:49 +0100

    qalfc.h
    added exponentiation

--- qalfc.h
+++ qalfc.h
@@ -3,28 +3,33 @@ float fsum(float a, float b);
 float fsub(float a, float b);
 float fmol(float a, float b);
 float fdiv(float a, float b);
+float fexpp(float a, unsigned float b);
 
 double dsum(double a, double b);
 double dsub(double a, double b);
 double dmol(double a, double b);
 double ddiv(double a, double b);
+double dexpp(double a, unsigned double b);
 
 /* INTEGRES NUMBER */
 int sum(int a, int b);
 int sub(int a, int b);
 int mol(int a, int b);
 int div(int a, int b);
+int expp(int a, unsigned int b);
 int rest(int a, int b);
 
 long lsum(long a, long b);
 long lsub(long a, long b);
 long lmol(long a, long b);
 long ldiv(long a, long b);
+long lexpp(long a, unsigned long b);
 long lrest(long a, long b);
 
 short ssum(short a, short b);
 short ssub(short a, short b);
 short smol(short a, short b);
 short sdiv(short a, short b);
+short sexpp(short a, unsigned short b);
 short srest(short a, short b);
 
